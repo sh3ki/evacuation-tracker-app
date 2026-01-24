8e99a3a37c7d819dfdd5b5b89a1d8a37c1b74db5
sh3ki <shekaigarcia@gmail.com>
2026-01-24 17:04:45 +0800

    Create EvacuationImageAdapter.kt

---
+++ app/src/main/java/com/evat/app/EvacuationImageAdapter.kt
@@ -0,0 +1,32 @@
+package com.evat.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class EvacuationImageAdapter(private val imageUrls: List<String>) :
+    RecyclerView.Adapter<EvacuationImageAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_evacuation_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(imageUrls[position])
+            .placeholder(R.drawable.map_placeholder)
+            .error(R.drawable.map_placeholder)
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int = imageUrls.size
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+}
